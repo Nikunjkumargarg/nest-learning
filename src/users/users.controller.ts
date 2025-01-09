0a5c6e3a9a590af98b7ce54bf40000d235d10d7d
Nikunj <nikunjkumargarg98@gmail.com>
2025-01-09 08:59:48 +0530

    add dependency injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,9 +17,12 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-params.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
+import { UsersService } from './providers/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UserService) {}
+
   @Get('/:id?')
   public getUsers(
     //@Param('id', ParseIntPipe) id: number | undefined,
