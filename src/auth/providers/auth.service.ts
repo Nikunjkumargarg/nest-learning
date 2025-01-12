352dd54b38490a0400f319a0dfaba537804aaa85
Nikunj <nikunjkumargarg98@gmail.com>
2025-01-12 09:19:33 +0530

    circular dependency add

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+  public login(email: string, password: string, id: string) {
+    const user = this.usersService.findOneById('12345');
+    return 'SAMPLE TOKEN';
+  }
+
+  public isAuth() {
+    return true;
+  }
+}
--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-params.dto';
+import { AuthService } from 'src/auth/providers/auth.service';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
   public findall(
     getUsersParamDto: GetUsersParamDto,
     limit: number,
     page: number,
   ) {
+    const isAuth = this.authService.isAuth();
+    console.log(isAuth);
     return [
       { getUsersParamDto, limit, page },
       {
