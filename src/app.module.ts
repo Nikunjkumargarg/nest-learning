c66a6d383f56f5905ccded304830eb4fbbb42536
Nikunj <nikunjkumargarg98@gmail.com>
2025-01-18 11:46:57 +0530

    typeorm intergration with nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,31 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      port: 5432,
+      username: 'postgres',
+      password: 'Nikunj@gauri32',
+      host: 'localhost',
+      database: 'nestjs-db',
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      //envFilePath: '[.env.development]',
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
